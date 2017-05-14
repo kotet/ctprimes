5c33836d3397c9e3f3c31dd31db62886f0b873c8
Kotet <kotet@users.noreply.github.com>
2017-05-14 14:04:00 +0900

    Declare ctPrimesLessThan, write unittests and documents

--- source/ctprimes.d
+++ source/ctprimes.d
@@ -56,3 +56,52 @@ public template ctPrimes(size_t length, T = size_t) if (isIntegral!T && 0 < leng
     auto primes3 = ctPrimes!0;
     assert(primes3 == []);
 }
+
+/**
+    Construct an array of all prime numbers less than N, using CTFE.
+    The type of the array is `typeof(N)[]`.
+
+    Params:
+        N = All elements of result are less than this value
+*/
+public template ctPrimesLessThan(N) if (isIntegral!(typeof(N)))
+{
+    enum typeof(N)[] ctPrimesLessThan = () {  }();
+}
+
+@safe unittest
+{
+    import std.random : uniform;
+
+    auto runtimevalue = uniform(0, 5);
+    static assert(!__traits(compiles, {
+            ctPrimes!runtimevalue; // Error: variable runtimevalue cannot be read at compile time
+        }));
+}
+
+@nogc @safe unittest
+{
+    static assert(__traits(compiles, {
+            ctPrimesLessThan!(cast(byte) 5);
+            ctPrimesLessThan!(cast(ubyte) 5);
+            ctPrimesLessThan!(cast(short) 5);
+            ctPrimesLessThan!(cast(int) 5);
+            ctPrimesLessThan!(cast(long) 5);
+        }));
+
+    static assert(!__traits(compiles, {
+            ctPrimesLessThan!(true);
+            ctPrimesLessThan!('a');
+            ctPrimesLessThan!(5f);
+        }));
+
+    auto primes1 = ctPrimesLessThan!(10);
+    static assert(is(typeof(primes1) == size_t[]));
+    assert(primes1 == [2, 3, 5, 7]);
+
+    auto primes2 = ctPrimesLessThan!2;
+    assert(primes2 == []);
+
+    auto primes3 = ctPrimesLessThan!0;
+    assert(primes3 == []);
+}
